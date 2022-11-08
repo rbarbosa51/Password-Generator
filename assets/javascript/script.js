c1e594980bba2af0fbea4cf4d4128e1374328de0
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-08 14:53:02 -0600

    In this commit: Added the javascript functionality to collect the Lower, Upper, numeric and special checkboxes values, and store them in their respective global variables. It console logs any changes in checked values.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var rangeInput = document.getElementById('passwordLength');
+var lowerCaseCheckbox = document.getElementById('lowercase');
+var upperCaseCheckbox = document.getElementById('uppercase');
+var numericCheckbox = document.getElementById('numeric');
+var SpecCharCheckbox = document.getElementById('specialcharacters');
+
 //Global Variables
 // passwordLength initial value is the center of the slider -- 68
 var passwordLength = 68;
+//Lowercase, uppercase, numeric, and Special Characters will be false by default
+var lowerCaseSelected = false;
+var upperCaseSelected = false;
+var numericSelected = false;
+var specialCaseSelected = false;
+
 function generatePassword() {
   return 'Test';
 }
@@ -27,4 +38,22 @@ rangeInput.addEventListener('change', () => {
   //Validating the input 
   passwordLength = rangeInput.value;
   console.log(`passwordLength value is: ${passwordLength}`);
-})
+});
+
+lowerCaseCheckbox.addEventListener('click', () => {
+  lowerCaseSelected = lowerCaseCheckbox.checked;
+  console.log(`Lower Case: ${lowerCaseSelected}`);
+});
+
+upperCaseCheckbox.addEventListener('click', () => {
+  upperCaseSelected = upperCaseCheckbox.checked;
+  console.log(`Upper Case: ${upperCaseSelected}`);
+});
+numericCheckbox.addEventListener('click', () => {
+  numericSelected = numericCheckbox.checked;
+  console.log(`Numeric: ${numericSelected}`);
+});
+SpecCharCheckbox.addEventListener('click', () => {
+  specialCaseSelected = SpecCharCheckbox.checked;
+  console.log(`Special Character: ${specialCaseSelected}`);
+});
